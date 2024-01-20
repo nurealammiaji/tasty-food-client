b37b0ed8d5711a2156cc42f45d4e88eb15a34059
nurealammiaji <nurealam.miaji@gmail.com>
2024-01-20 09:30:38 +0600

    manage item form added

--- src/pages/Dashboard/Admin/AddItems.jsx
+++ src/pages/Dashboard/Admin/AddItems.jsx
@@ -12,20 +12,20 @@ const AddItems = () => {
             <div className="p-10">
                 <SectionHeading subHeading={"What's New ?"} heading={"Add An Item"} width={'w-8/12 md:w-6/12'}></SectionHeading>
                 <br /><br />
-                <div>
-                    <form className="p-5 bg-base-200">
-                        <div className="form-control w-full">
+                <div className="w-full lg:w-[600px] xl:w-[992px]">
+                    <form className="p-10 bg-base-200">
+                        <div className="w-full form-control">
                             <div className="label">
                                 <span className="label-text">Recipe Name *</span>
                             </div>
-                            <input type="text" placeholder="Type here" className="input input-bordered w-full" />
+                            <input type="text" placeholder="Type here" className="w-full input input-bordered" />
                         </div>
-                        <div className="flex-row md:flex gap-5 mt-3">
-                            <div className="form-control w-full max-w-xs">
+                        <div className="flex-row gap-5 mt-3 md:flex">
+                            <div className="w-full md:w-6/12 form-control">
                                 <div className="label">
                                     <span className="label-text">Category *</span>
                                 </div>
-                                <select className="select select-bordered w-full max-w-xs">
+                                <select className="w-full select select-bordered">
                                     <option>Salads</option>
                                     <option>Pizzas</option>
                                     <option>Soups</option>
@@ -33,27 +33,27 @@ const AddItems = () => {
                                     <option>Drinks</option>
                                 </select>
                             </div>
-                            <div className="form-control w-full max-w-xs">
+                            <div className="w-full mt-3 md:mt-0 md:w-6/12 form-control">
                                 <div className="label">
                                     <span className="label-text">Price *</span>
                                 </div>
-                                <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+                                <input type="text" placeholder="Type here" className="w-full input input-bordered" />
                             </div>
                         </div>
-                        <div className="form-control w-full mt-3">
+                        <div className="w-full mt-3 form-control">
                             <div className="label">
                                 <span className="label-text">Recipe Details *</span>
                             </div>
                             <textarea className="textarea textarea-bordered" placeholder="Recipe Details"></textarea>
                         </div>
-                        <div className="form-control w-full mt-3">
+                        <div className="w-full mt-3 form-control">
                             <div className="label">
                                 <span className="label-text">Recipe Image *</span>
                             </div>
-                            <input type="file" placeholder="You can't touch this" className="file-input file-input-bordered w-full"/>
+                            <input type="file" placeholder="You can't touch this" className="w-full file-input file-input-bordered"/>
                         </div>
                         <br />
-                        <button type="submit" className="btn bg-[#D1A054]">Add Item <PiForkKnifeBold/></button>
+                        <button type="submit" className="btn bg-[#D1A054]">Add Item <PiForkKnifeBold /></button>
                     </form>
                 </div>
             </div>
--- src/pages/Dashboard/Admin/ManageItems.jsx
+++ src/pages/Dashboard/Admin/ManageItems.jsx
@@ -11,8 +11,45 @@ const ManageItems = () => {
             <div className="p-10">
                 <SectionHeading subHeading={"Hurry Up"} heading={"Manage All Items"} width={'w-8/12 md:w-6/12'}></SectionHeading>
                 <br /><br />
-                <div>
-                    
+                <div className="w-full lg:w-[600px] xl:w-[992px]">
+                    <form className="p-10 bg-base-200">
+                        <div className="w-full form-control">
+                            <div className="label">
+                                <span className="label-text">Recipe Name *</span>
+                            </div>
+                            <input type="text" placeholder="Type here" className="w-full input input-bordered" />
+                        </div>
+                        <div className="flex-row gap-5 mt-3 md:flex">
+                            <div className="w-full md:w-6/12 form-control">
+                                <div className="label">
+                                    <span className="label-text">Category *</span>
+                                </div>
+                                <select className="w-full select select-bordered">
+                                    <option>Salads</option>
+                                    <option>Pizzas</option>
+                                    <option>Soups</option>
+                                    <option>Desserts</option>
+                                    <option>Drinks</option>
+                                </select>
+                            </div>
+                            <div className="w-full mt-3 md:mt-0 md:w-6/12 form-control">
+                                <div className="label">
+                                    <span className="label-text">Price *</span>
+                                </div>
+                                <input type="text" placeholder="Price" className="w-full input input-bordered" />
+                            </div>
+                        </div>
+                        <div className="w-full mt-3 form-control">
+                            <div className="label">
+                                <span className="label-text">Recipe Details *</span>
+                            </div>
+                            <textarea className="textarea textarea-bordered" placeholder="Recipe Details" rows={5}></textarea>
+                        </div>
+                        <br />
+                        <div className="text-center">
+                            <button type="submit" className="btn bg-[#D1A054]">Update Item</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
